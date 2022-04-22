d1270c3fd04c6c4bb13027ef6a5f45f5610cd957
Jeremie Helme <jeremiehelme@gmail.com>
2022-04-22 16:59:44 +0200

    Component : CurrencyTable

---
+++ components/CurrencyTable.tsx
@@ -0,0 +1,30 @@
+import { Component } from 'react'
+import DataTable from 'react-data-table-component';
+
+
+
+
+class CurrencyTable extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    const { currencies, columns } = this.props;
+
+    return (
+      currencies.length > 0 && (
+
+        <DataTable
+          columns={columns}
+          data={currencies}
+        />
+
+      )
+    );
+  }
+}
+
+export default CurrencyTable
